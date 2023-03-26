63333cae1163c82080feeef8520cc13b45416c93
ramonprog <programador.ramon@gmail.com>
2023-03-26 16:42:14 -0300

    feat: login page created and styled

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,47 @@
+import { Container } from './styles';
+
+import Logo from '../../assets/Logo_menor.png';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  return (
+    <Container>
+      <div className="login">
+        <div className="login-area">
+          <img src={Logo} alt="logo do sistema de chamadas" />
+        </div>
+
+        <form>
+          <h1>Entrar</h1>
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={form.email}
+            onChange={(event) =>
+              setForm({ ...form, email: event.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="********"
+            value={form.password}
+            onChange={(event) =>
+              setForm({ ...form, password: event.target.value })
+            }
+          />
+
+          <button type="submit">Acessar</button>
+        </form>
+
+        <Link to={'/register'}>Criar uma conta</Link>
+      </div>
+    </Container>
+  );
 };
 
 export default SignIn;
